738a323c3bef1b4b4faf1218f9afae01796c62a1
Budko Vyacheslav <budko.v1989@gmail.com>
2016-10-06 12:28:06 +0300

    compleate migration for daemon job

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=demo',
+    'username' => 'demo',
+    'password' => 'demo',
     'charset' => 'utf8',
 ];
--- migrations/m161006_091041_create_daemon_job_table.php
+++ migrations/m161006_091041_create_daemon_job_table.php
@@ -14,6 +14,16 @@ class m161006_091041_create_daemon_job_table extends Migration
     {
         $this->createTable('daemon_job', [
             'id' => $this->primaryKey(),
+            'task' => $this->string(255),
+            'arguments' => $this->text(),
+            'status' => $this->string(30),
+            'progress' => $this->string(30),
+            'daemonId' => $this->integer(20),
+            'result' => $this->text(),
+            'errors' => $this->text(),
+            'created' => $this->timestamp("CURRENT_TIMESTAMP"),
+            'started' => $this->timestamp(),
+            'finished' => $this->timestamp(),
         ]);
     }
 
